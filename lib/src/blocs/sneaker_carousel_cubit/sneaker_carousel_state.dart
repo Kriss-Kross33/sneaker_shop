707f40373dd1ef59824ba82ac667afa2195ceb04
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-03-12 16:29:44 +0000

    remove redundant code.

--- lib/src/blocs/sneaker_carousel_cubit/sneaker_carousel_state.dart
+++ lib/src/blocs/sneaker_carousel_cubit/sneaker_carousel_state.dart
@@ -1,28 +1,8 @@
 part of 'sneaker_carousel_cubit.dart';
 
-// class SneakerCarouselState extends Equatable {
-//   // final int index;
-//   // final SneakerModel? sneaker;
-//   // const SneakerCarouselState({
-//   //   required this.index,
-//   //   this.sneaker,
-//   // });
-//   const SneakerCarouselState();
-
-//   // @override
-//   // List<Object?> get props => [index, sneaker];
-
-//   @override
-//   List<Object> get props => [];
-// }
-
 class SneakerCarouselState extends Equatable {
-  // final int index;
   final SneakerModel sneaker;
-  // const SneakerCarouselState({
-  //   required this.index,
-  //   this.sneaker,
-  // });
+
   const SneakerCarouselState({required this.sneaker});
 
   SneakerCarouselState copyWith({SneakerModel? sneaker}) {
@@ -34,19 +14,3 @@ class SneakerCarouselState extends Equatable {
   @override
   List<Object> get props => [sneaker];
 }
-
-// class SneakerCarouselInitialState extends SneakerCarouselState {
-//   const SneakerCarouselInitialState();
-// }
-
-// class SneakerCarouselChangedState extends SneakerCarouselState {
-//   final int index;
-//   final SneakerModel sneaker;
-//   const SneakerCarouselChangedState({
-//     required this.index,
-//     required this.sneaker,
-//   });
-
-//   @override
-//   List<Object> get props => [index, sneaker];
-// }
