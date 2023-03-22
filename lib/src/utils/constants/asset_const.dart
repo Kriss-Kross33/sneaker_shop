159b285454cbae855cda0cbeac2008eae78b0fe8
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-03-22 18:11:15 +0000

    add nike zoom assets

--- lib/src/utils/constants/asset_const.dart
+++ lib/src/utils/constants/asset_const.dart
@@ -8,10 +8,14 @@ class AssetConsts {
   static const String nikeAirJordanCyan = '$_imagePath/J_cyan.png';
   static const String nikeAirJordanRed = '$_imagePath/J_red.png';
   static const String nikeAirJordanGreen = '$_imagePath/J_green.png';
-  // static const String nikeLogoWhite = '$_imagePath/nike_w.png';
-  // static const String nikeLogoWhite = '$_imagePath/nike_w.png';
-  // static const String nikeLogoWhite = '$_imagePath/nike_w.png';
-  // static const String nikeLogoWhite = '$_imagePath/nike_w.png';
+
+  static const String nikeZoomLemonGreen = '$_imagePath/N_lemon_green.png';
+  static const String nikeZoomViolet = '$_imagePath/N_violet.png';
+  static const String nikeZoomBlack = '$_imagePath/N_black.png';
+  static const String nikeZoomDeepGreen = '$_imagePath/N_deep_green.png';
+
+  static const String nikeLogoDark = '$_imagePath/nike_logo_dark.png';
+  static const String nikeLogoLight = '$_imagePath/nike_logo_light.png';
 
   static List<String> allAssets() {
     return <String>[
@@ -21,6 +25,10 @@ class AssetConsts {
       nikeAirJordanCyan,
       nikeAirJordanRed,
       nikeAirJordanGreen,
+      nikeZoomBlack,
+      nikeZoomDeepGreen,
+      nikeZoomLemonGreen,
+      nikeZoomViolet,
     ];
   }
 }
