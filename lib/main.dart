6abc6807a1651b7692c8188a220ac91717eee91c
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-03-23 05:08:00 +0000

    rename  to

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sneaker_shop/src/blocs/blocs.dart';
-import 'package:sneaker_shop/src/ui/home/home_screen.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sneaker_shop/src/sneaker_app.dart';
 import 'package:sneaker_shop/src/utils/utils.dart';
 
-void main() {
+Future<void> main() async {
   final binding = WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
       overlays: [SystemUiOverlay.bottom]);
+  HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory());
 
   binding.addPostFrameCallback((_) async {
     BuildContext context = binding.renderViewElement!;
@@ -16,31 +18,5 @@ void main() {
       precacheImage(AssetImage(asset), context);
     }
   });
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<SneakerCarouselCubit>(
-          create: (context) => SneakerCarouselCubit(),
-        ),
-        BlocProvider<SneakerColorSelectorCubit>(
-          create: (context) => SneakerColorSelectorCubit(),
-        ),
-        BlocProvider<SneakerSizeSelectorCubit>(
-          create: (context) => SneakerSizeSelectorCubit(),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.defaultTheme,
-        home: const HomeScreen(),
-      ),
-    );
-  }
+  runApp(const SneakerApp());
 }
---
+++ lib/src/sneaker_app.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sneaker_shop/src/blocs/blocs.dart';
+import 'package:sneaker_shop/src/utils/utils.dart';
+
+class SneakerApp extends StatelessWidget {
+  const SneakerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<SneakerThemeCubit>(
+          create: (context) => SneakerThemeCubit(),
+        ),
+        BlocProvider<SneakerCarouselCubit>(
+          create: (context) => SneakerCarouselCubit(),
+        ),
+        BlocProvider<SneakerColorSelectorCubit>(
+          create: (context) => SneakerColorSelectorCubit(),
+        ),
+        BlocProvider<SneakerSizeSelectorCubit>(
+          create: (context) => SneakerSizeSelectorCubit(),
+        ),
+      ],
+      child: BlocBuilder<SneakerThemeCubit, SneakerThemeState>(
+        builder: (context, state) {
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            theme: state.themeMode == SneakerThemeMode.darkMode
+                ? AppTheme.defaultTheme
+                : AppTheme.lightTheme,
+            routeInformationParser: AppRouter.router.routeInformationParser,
+            routerDelegate: AppRouter.router.routerDelegate,
+          );
+        },
+      ),
+    );
+  }
+}
